f6f805862d30f70fb22422b7bb06b68a3199a6dc
Clbt <cmichel.44@free.fr>
2013-11-22 17:32:47 +0100

    Update math.adb
    Implementation du package entier: 
    a verifier Bezier & points de contrôle -> checker l'usage des fonctions dans les autres .adb

--- algo1_generation/math.adb
+++ algo1_generation/math.adb
@@ -20,10 +20,34 @@ package body Math is
       return R;
    end;
 
+
+--C1 et C2 points de contrôle?
+--cubique
    procedure Bezier(P1, C1, C2, P2 : Point2D ; Nb_Points : Positive ;
                     Points : out Liste) is
+  Index : Integer := 0;
+  Bezier : Point2D;	 
+ 
    begin
-	   null;
+	  while Index <= 1 loop 
+	 Bezier := C1*(1-Index)^3 + C2*3*(1-Index) + P1*3*Index^2(1-Index) + P2*Index^3;
+	 Insertion_Queue (Points, Bezier);
+	 Index:=Index+1/Nb_Points;
+	 
+	 end loop;
+	 
+	    end;
+
+--quadratique
+--Pb: points de controle. -> lequel jarter/pourquoi? 
+ procedure Bezier(P1, C1, C2, P2 : Point2D ; Nb_Points : Positive ;
+                    Points : out Liste) is
+   begin
+   	  while Index <= 1 loop 
+	 Bezier := C1*(1-Index)^2 + C2*2*Index*(1-Index) + P1*Index^2;
+	 Insertion_Queue (Points, Bezier);
+	 Index:=Index+1/Nb_Points;
+	 
    end;
 
    
