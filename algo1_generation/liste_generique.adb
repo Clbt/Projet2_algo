c99b89968c51e9c9049c434bb317b922c2026f99
Clbt <cmichel.44@free.fr>
2013-11-26 10:43:06 +0100

    Update liste_generique.adb
    Fin de l'implementation du package -> checker InsertionQueue & Parcourir Par Couples

--- algo1_generation/liste_generique.adb
+++ algo1_generation/liste_generique.adb
@@ -2,6 +2,8 @@ with Ada.Unchecked_Deallocation;
 
 package body Liste_Generique is
 
+   ListeVide : exception ;
+
    procedure Vider(L : in out Liste) is
    begin
 	   null;
@@ -17,13 +19,20 @@ package body Liste_Generique is
    end;
 
    procedure Insertion_Queue(L : in out Liste ; E : Element) is
+   PointeurDeLambiance : Pointeur := null;
+   
    begin
-	   null;
+   
+  
+   PointeurDeLambiance := new Cellule' (Contenu => Element , Suivant => null);
+   L.Fin.all.Suivant := PointeurDeLambiance; -- on remplace Suivant de l'avant dernier element par un pointeur sur l'element insere
+   L.Fin := PointeurDeLambiance; --Fin pointe maintenant sur la derniere cellule
+   L.Taille := L.Taille + 1;
    end;
 
    procedure Parcourir (L : Liste) is
    
-   Courant= Pointeur := L.Debut;
+   Courant : Pointeur := L.Debut;
    
    begin
 	  
@@ -34,28 +43,50 @@ package body Liste_Generique is
    end;
 
    procedure Parcourir_Par_Couples(L : Liste) is
+   
+   Courant : Pointeur := L.Debut;
+   Suivant : Pointeur := L.Debut.all.Suivant;
+   Taille : Natural := L.Taille;
+   
    begin
-	   null;
+	while Suivant /= L.Fin loop
+	Traiter (Courant, Suivant);
+	Courant:=Suivant;
+	Suivant:=Suivant.all.Suivant;
+	end loop;
+	
    end;
 
    procedure Fusion(L1 : in out Liste ; L2 : in out Liste) is
    begin
-	   null;
+	   InsertionQueue ( L1, L2.Debut.all);
+	   L2 := null;
    end;
 
    function Taille(L : Liste) return Natural is
+   
+   Courant : Pointeur := L.Debut;
    begin
+      while Courant /= L.Fin loop
+      	L.Taille := L.Taille+1;
+      end loop;
       return L.Taille;
    end;
 
    function Tete(L : Liste) return Element is
    begin
-      return L.Debut.Contenu;
+   	if L.Taille /=0 then
+      		return L.Debut.Contenu;
+      	else raise ListeVide;
+      	end if;
    end;
 
    function Queue(L : Liste) return Element is
    begin
-      return L.Fin.Contenu;
+      if L.Taille /=0 then
+      		return L.Fin.Contenu;
+      else raise ListeVide;
+      end if;
    end;
 
 end;
